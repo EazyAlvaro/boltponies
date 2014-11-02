c19328722ac2f955fd95459d2db5dd93fd3ece90
Gawain Lynch <gawain.lynch@gmail.com>
2014-11-02 19:03:30 +0000

    Constructor not needed for fronend asset insertion

--- Extension.php
+++ Extension.php
@@ -12,19 +12,11 @@ class Extension extends BaseExtension
         return "BoltPonies";
     }
 
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        if ($this->app['config']->getWhichEnd() == 'frontend') {
-            $this->app['htmlsnippets'] = true;
-            $this->app['twig.loader.filesystem']->prependPath( __DIR__ . '/twig');
-        }
-    }
-
     public function initialize()
     {
-        $this->addJavascript('assets/browserponies.js', true);
-        $this->addJavascript('assets/ponycfg.js', true);
+        if ($this->app['config']->getWhichEnd() == 'frontend') {
+            $this->addJavascript('assets/browserponies.js', true);
+            $this->addJavascript('assets/ponycfg.js', true);
+        }
     }
 }
